f73cfac48bed67cff9a1eb01ba9f121596ca93a6
Crowdbotics <team@crowdbotics.com>
2020-04-13 17:13:48 +0000

    Update models

--- backend/dodo_15804/wsgi.py
+++ backend/dodo_15804/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dodo_15804.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dodo_15804.settings")
 
 application = get_wsgi_application()
